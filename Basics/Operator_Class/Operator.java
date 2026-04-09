fb407af4658f7b483578ad094442b856313a921b
Abhishek16758 <A8728517@gmail.com>
2026-04-09 12:18:18 +0530

    Add eligibility check based on percentage input

--- Basics/Operator_Class/Operator.java
+++ Basics/Operator_Class/Operator.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class Operator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Apni percentage enter karein: ");
+        double percentage = sc.nextDouble();
+
+        // Agar 60% se zyada hai toh eligible
+        if (percentage >= 60.0) {
+            System.out.println("Badhai ho! Aap interview ke liye eligible hain.");
+        } else if (percentage >= 50.0 && percentage < 60.0) {
+            System.out.println("Aap waitlist mein hain.");
+        } else {
+            System.out.println("Agli baar mehnat karein.");
+        }
+
+        sc.close();
+    }
+}
